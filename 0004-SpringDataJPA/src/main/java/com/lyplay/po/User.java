e51d71462ed097de6238861af8b04ede0348a8cc
lyplay <i@lyplay.com>
2017-07-23 07:56:33 +0800

    codes update
    Change-Id: I1719bf3c00c4b316dc888c9b5563661136fb2ebf

--- 0004-SpringDataJPA/src/main/java/com/lyplay/po/User.java
+++ 0004-SpringDataJPA/src/main/java/com/lyplay/po/User.java
@@ -3,13 +3,14 @@ package com.lyplay.po;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 	
 	@Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	
 	@Column(name = "name")
--- 0004-SpringDataJPA/src/test/java/com/lyplay/DemoApplicationTests.java
+++ 0004-SpringDataJPA/src/test/java/com/lyplay/DemoApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.lyplay.dao.UserJpaDao;
 import com.lyplay.po.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes=App.class)
+@WebAppConfiguration
 public class DemoApplicationTests {
 
 	@Autowired
