1e803083b041d55e196720fe093b9bb1f942322d
Swin.Liu <swin.liu@gpayroll.com>
2017-09-01 18:45:54 +0800

    MQ update
    Change-Id: I10709c0ac3e1e2d2b5939a21386ad89325d66189

--- 0008-SpringBootActiveMQ/src/main/java/com/lyplay/App.java
+++ 0008-SpringBootActiveMQ/src/main/java/com/lyplay/App.java
@@ -1,17 +1,9 @@
 package com.lyplay;
 
-import javax.jms.Queue;
-import javax.jms.Topic;
-
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.command.ActiveMQQueue;
-import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jms.core.JmsMessagingTemplate;
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
@@ -24,31 +16,5 @@ public class App extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(App.class);
     }
-    
-    
-    
-    @Bean  
-    public Queue queue(){  
-    	return new ActiveMQQueue("mytest.queue");  
-    }
-    
-    @Bean  
-    public Topic topic(){  
-    	return new ActiveMQTopic("mytest.topic");  
-    }
-    
-    
-	@Bean
-	public ActiveMQConnectionFactory connectionFactory() {
-		// 此链接信息可放入配置文件中
-		return new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
-	}
-
-	@Bean
-	public JmsMessagingTemplate jmsMessagingTemplate(ActiveMQConnectionFactory connectionFactory) {
-		System.out.println("get JmsMessagingTemplate");
-		return new JmsMessagingTemplate(connectionFactory);
-	}
-    
     
 }
--- 0008-SpringBootActiveMQ/src/main/java/com/lyplay/controller/LoginController.java
+++ 0008-SpringBootActiveMQ/src/main/java/com/lyplay/controller/LoginController.java
@@ -1,8 +1,9 @@
 package com.lyplay.controller;
 
-import javax.jms.Queue;
-import javax.jms.Topic;
+import javax.jms.Destination;
 
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -21,23 +22,19 @@ public class LoginController {
     private StringRedisTemplate stringRedisTemplate;
 	
 	@Autowired
-	private Topic topic;
-	
-	@Autowired
-	private Queue queue;
-	
-	@Autowired
 	private Producer producer;
 	
 	@RequestMapping(value = "/api/mq", method = RequestMethod.GET)
 	@ResponseBody
 	public String topic() {
-		for(int i=0; i<100; i++){  
-        	System.out.println("send queue is Swin.Liu!!! " + i);
-            producer.sendMessage(queue, "myname queue is Swin.Liu!!! " + i);  
-            System.out.println("send topic is Swin.Liu!!! " + i);
-            producer.sendMessage(topic, "myname topic is Swin.Liu!!! " + i);  
-        }  
+		
+		Destination queueDestination = new ActiveMQQueue("mytest.queue");  
+        Destination topicDestination = new ActiveMQTopic("mytest.topic");  
+          
+    	System.out.println("send queue is Swin.Liu!!! ");
+        producer.sendMessage(queueDestination, "myname queue is Swin.Liu!!! " + Math.random());  
+        System.out.println("send topic is Swin.Liu!!! ");
+        producer.sendMessage(topicDestination, "myname topic is Swin.Liu!!! " + Math.random());  
         return "sent mq messages.";
     }
 	
